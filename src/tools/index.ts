54f8cb5cebdcf347a533ccb6d4a9b156ade60741
Simone Taeggi <startae14@gmail.com>
2025-02-17 16:19:57 +0100

    tool index file to export all the tools

---
+++ src/tools/index.ts
@@ -0,0 +1,6 @@
+import { generateImage } from './generateImage'
+import { dadJoke } from './dadJokes'
+import { italianJoke } from './italianJokes'
+import { reddit } from './reddit'
+
+export const tools = [generateImage, reddit, dadJoke, italianJoke]
